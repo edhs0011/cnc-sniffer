d989c66bb84b200e9de1a5c5380344effb1a9d25
Eddie S Hsieh <eddie_s_hsieh@trend.com.tw>
2016-12-12 18:10:55 +0800

    use thread to fork connection

--- main.py
+++ main.py
@@ -2,35 +2,58 @@ import socket
 import sys
 import struct
 import threading
+import re
+import time
+import logging
+FORMAT = "%(asctime)-15s %(levelname)-8s %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT, filename='/var/log/cnc-sniffer/cnc.log')
+logger = logging.getLogger(__name__)
 
 class Conection:
     def __init__(self, server):
         self.addr = server[0]
         self.port = server[1]
         self.atk_dict = dict()
-        self.atk_dict["cnc"] = self.addr
 
     def run(self):
-        # Create a TCP/IP socket
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # Connect the socket to the port where the server is listening
-        server_address = (self.addr, self.port)
-        print >>sys.stderr, 'connecting to %s port %s' % server_address
-        self.sock.connect(server_address)
-
-        try:
-            # Send data
-            print >>sys.stderr, 'sending'
-            self.sock.sendall(b'\x00\x00\x00\x01')
-            self.sock.sendall(b'\x00')
-
+        m = re.match('[0-9].[0-9].[0-9].[0-9]', self.addr)
+        if m is not None:
+            self.domain = ""
+        else:
+            self.domain = self.addr
             while True:
-                self.parse_attack()
+                try:
+                    self.addr = socket.gethostbyname(self.addr)
+                    break
+                except:
+                    time.sleep(5)
+                    logger.debug("retry to get the IP from DNS %s" % self.addr)
+                    continue
 
-        finally:
-            print >>sys.stderr, 'closing socket'
-            self.sock.close()
+        self.atk_dict["cnc"] = self.addr
+        self.atk_dict["domain"] = self.domain
+
+        while True:
+            try:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_address = (self.addr, self.port)
+                logger.debug('connecting to %s port %s' % server_address)
+                self.sock.connect(server_address)
+                # Send data
+                logger.info('successfully connected to CNC: %s' % self.addr)
+                self.sock.sendall(b'\x00\x00\x00\x01')
+                self.sock.sendall(b'\x00')
+
+                while True:
+                    self.parse_attack()
+            except socket.error, msg:
+                time.sleep(5)
+                logger.error(msg)
+                logger.error("reconnect to the CNC server %s" % self.addr)
+                continue
+            finally:
+                logger.debug('closing socket')
+                self.sock.close()
 
     def read_int_from_bytes(self, bytes):
         data = self.sock.recv(bytes)
@@ -51,50 +74,49 @@ class Conection:
     def parse_attack(self):
         pkg_len = self.read_int_from_bytes(2)
         self.atk_dict["pkg_len"] = pkg_len
-        print "total len %d" % pkg_len
+        logger.debug("total len %d" % pkg_len)
 
         atk_duration = self.read_int_from_bytes(4)
         self.atk_dict["atk_duration"] = atk_duration
-        print "duration %d" % atk_duration
+        logger.debug("duration %d" % atk_duration)
 
         atk_id = self.read_int_from_bytes(1)
         self.atk_dict["atk_id"] = atk_id
-        print "attack ID %d" % atk_id
+        logger.debug("attack ID %d" % atk_id)
 
         atk_target = self.read_int_from_bytes(1)
         self.atk_dict["atk_target"] = atk_target
-        print "target %d" % atk_target
+        logger.debug("target %d" % atk_target)
 
         # read IP
         atk_ip = map(lambda x: str(x), self.read_ip_from_bytes())
-        print "IP"
-        print atk_ip
+        logger.debug(atk_ip)
 
         # IP mask
         atk_ip_mask = self.read_int_from_bytes(1)
-        print "IP mask %d" % atk_ip_mask
+        logger.debug("IP mask %d" % atk_ip_mask)
 
         self.atk_dict["atk_ip"] = "{0}/{1}".format(".".join(atk_ip), atk_ip_mask)
 
         # opt len
         atk_opt_len = self.read_int_from_bytes(1)
-        print "opt len %d" % atk_opt_len
+        logger.debug("opt len %d" % atk_opt_len)
 
         atk_opt = []
         for i in range(atk_opt_len):
             atk_opt_key = self.read_int_from_bytes(1)
-            print "opt key %d" % atk_opt_key
+            logger.debug("opt key %d" % atk_opt_key)
 
             opt_val_len = self.read_int_from_bytes(1)
-            print "opt val len %d" % opt_val_len
+            logger.debug("opt val len %d" % opt_val_len)
 
             atk_opt_data = self.sock.recv(opt_val_len)
-            print bytes.decode(atk_opt_data)
+            logger.debug(bytes.decode(atk_opt_data))
 
             atk_opt.append((atk_opt_key, atk_opt_data))
 
         self.atk_dict["atk_opt"] = atk_opt
-        print self.atk_dict
+        logger.info(self.atk_dict)
 
 def sniffer(cnc_servers):
     for server in cnc_servers:
@@ -105,8 +127,35 @@ def sniffer(cnc_servers):
 
 def main():
     cnc_servers = [
-        ("35.162.249.35", 23),
-        ("35.162.249.35", 23)
+        ("network.santasbigcandycane.cx", 23),
+        ("cnc.disabled.racing", 23),
+        ("gay.disabled.racing", 23),
+        ("penis.disabled.racing", 23),
+        ("b0ts.xf0.pw", 23),
+        ("swinginwithme.ru", 23),
+        ("imscaredaf.xyz", 23),
+        ("kankerc.queryhost.xyz", 23),
+        ("meme.icmp.online", 23),
+        ("our.bklan.ru", 23),
+        ("heis.lateto.work", 23),
+        ("mufoscam.org", 23),
+        ("netwxrk.org", 23),
+        ("q5f2k0evy7go2rax9m4g.ru", 23),
+        ("check.securityupdates.us", 23),
+        ("cnc.routersinthis.com", 23),
+        ("dongs.disabled.racing", 23),
+        ("dongs.icmp.online", 23),
+        ("ftp.timeserver.host", 23),
+        ("ftp.xenonbooter. xyz", 23),
+        ("hightechcrime.club", 23),
+        ("irc.xf0.pw", 23),
+        ("listen.routersinthis.com", 23),
+        ("listen.xenonbooter.xyz", 23),
+        ("loadsecure.pw", 23),
+        ("lol.disabled.racing", 23),
+        ("timeserver.host", 23),
+        ("tr069.online", 23),
+        ("www.mufoscam.org", 23)
     ]
     sniffer(cnc_servers)
     while True:
